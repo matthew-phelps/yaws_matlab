c222fccd188a12deabb50ee30c6574eb4baf56a6
Matthew Phelps <mdp553@hotmail.com>
2015-07-29 09:41:59 +0200

    MCMC run + beta params
    Results of MCMC v2 run

---
+++ Laura's Folder/BetaParams.R
@@ -0,0 +1,10 @@
+## Determine variance
+mean <- 85.5
+variance <- ((mean-78.2)*sqrt(250)/1.96)^2
+
+estBetaParams <- function(mu, var) {
+       alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
+       beta <- alpha * (1 / mu - 1)
+       return(params = list(alpha = alpha, beta = beta))
+   }
+estBetaParams(mean, variance)
